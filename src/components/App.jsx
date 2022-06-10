95edf778b135104513a8f1f1cadb9b9f22d6977f
Some14u <some1@email.ua>
2022-06-10 22:02:23 +0300

    Refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import ImageGallery from "./ImageGallery";
 import Modal from "./Modal";
 import Button from "./Button";
-import Loader from "./Loader";
+import Loader from "./Loader"
 import s from "./App.module.css";
 
 export class App extends React.Component {
@@ -17,15 +17,14 @@ export class App extends React.Component {
       query: "q",
       page: "page",
       perPage: "per_page",
+      previewURL: "webformatURL",
     },
     defaultQuery: {
       key: process.env.REACT_APP_SEARCH_KEY,
       image_type: "photo",
       orientation: "horizontal",
-
       per_page: 12,
     },
-    loading: false,
   }
 
   static modalBase = document.getElementById("modal");
@@ -35,56 +34,57 @@ export class App extends React.Component {
     itemsAvailable: 0,
     page: 1,
     modal: { visible: false },
+    isLoading: false,
+    error: null,
   };
 
-  async fetchImages (list) {
-    var promises, responces
-    // form and request urls
-    promises = list.map(item => fetch(item.webformatURL));
-    responces = await Promise.allSettled(promises);
-    // form and request blobs
-    promises = responces.map(r => r.value?.blob());
-    responces = await Promise.allSettled(promises);
-    // form and return local url's
-    return responces.map(r => r.value && URL.createObjectURL(r.value));
-  };
-
-  doSearch = async (searchString) => {
+  get requestParams() {
     const api = this.constructor.api;
-
-    var items = searchString ? [] : this.state.items;
-    const page = searchString ? 1 : this.state.page;
-    
-    this.setState({ loading: true, items });
-
-    const query = {
+    const { searchString, page } = this.state;
+    return {
       ...api.defaultQuery,
-      [api.fields.query]: searchString || this.state.searchString,
+      [api.fields.query]: searchString,
       [api.fields.page]: page,
     };
-    
-    const { data } = await axios.get(api.url, { params: query });
-    const newItems = data ? data[api.fields.items] : [];
+  }
 
-    const localImgURLs = await this.fetchImages(data[api.fields.items]);
-    newItems.forEach((item, idx) => item.webformatURL = localImgURLs[idx] || "");
+  get nextPageAvailable() {
+    const [api, { items, page, itemsAvailable }] = [this.constructor.api, this.state];
+    return items.length!==0 && page * api.defaultQuery[api.fields.perPage] < itemsAvailable;
+  }
 
-    items = [...items, ...newItems];
+  async fetchImages() {
+    const api = this.constructor.api;
+    // Get images list
+    const { data } = await axios.get(api.url, { params: this.requestParams });
+    const items = data ? data[api.fields.items] : [];
+    // For each entrie predownload image and obtain local url
+    const promises = items.map(item => axios.get(item[api.fields.previewURL], { responseType: 'blob' }));
+    const responces = await Promise.allSettled(promises);
+    const localURLs = responces.map(r => r.value && URL.createObjectURL(r.value.data));
+    // Substitude remote urls to local ones
+    items.forEach((item, idx) => item[api.fields.previewURL] = localURLs[idx] || "");
 
-    const scrollTo = searchString ? 0 : (document.getElementById("gallery")?.scrollHeight || 0);
+    return [ items, data[api.fields.total] ];
+  };
 
+  doSearch = async () => {
+    this.setState({ isLoading: true });
+
+    const [items, itemsAvailable] = await this.fetchImages();
+
+    const scrollTo = document.getElementById("gallery")?.scrollHeight || 0;
     this.setState(
       {
-        loading: false,
-        searchString: searchString || this.state.searchString,
-        items,
-        page,
-        itemsAvailable: data[api.fields.total],
+        isLoading: false,
+        items: [...this.state.items, ...items],
+        itemsAvailable
       },
       () => window.scrollTo({ top: scrollTo + 8, behavior: scrollTo === 0 ? "instant" : "smooth" }),
     );
   }
-  
+
+
   showModal = (url, tags) => {
     const modal = { visible: true, url, tags };
     this.setState({modal});
@@ -96,28 +96,32 @@ export class App extends React.Component {
   }
 
   advancePage = () => {
-    this.setState(
-      oldState => ({ page: oldState.page + 1 }),
-      () => this.doSearch()
+    this.setState( oldState => ({ page: oldState.page + 1 }),
     );
   }
 
-  get nextPageAvailable() {
-    const [api, { items, page, itemsAvailable }] = [this.constructor.api, this.state];
-    return items.length!==0 && page * api.defaultQuery[api.fields.perPage] < itemsAvailable;
+  onSubmit = (searchString) => {
+    this.setState({ searchString, items: [], itemsAvailable: 0, page: 1 });
   }
 
+  componentDidUpdate(_, prevState) {
+    const state = this.state;
+    if (
+      state.searchString !== prevState.searchString ||
+      state.page !== prevState.page ||
+      state.items.length < prevState.items.length
+    ) this.doSearch().catch( e => this.setState({error: e}) );
+  }
 
   render() {
-    const { items, modal, loading } = this.state;
+    const { items, modal, isLoading, error } = this.state;
     return (
       <div className={s.app}>
-        <Searchbar onSubmit={this.doSearch} />
-        <ImageGallery items={items} showModalCb={this.showModal} />
-
-        {loading && <Loader />}
-        {this.nextPageAvailable && !loading && <Button onClickCb={this.advancePage}>Load more</Button>}
-        
+        <Searchbar onSubmit={this.onSubmit} />
+        { error && <><h2>Something went wrong. Please, reload page.</h2><p>{error}</p></> }
+        { !error && <ImageGallery items={items} showModalCb={this.showModal} /> }
+        { isLoading && <Loader /> }
+        { !isLoading && this.nextPageAvailable && <Button onClickCb={this.advancePage}>Load more</Button> }
         <Modal base={this.constructor.modalBase} {...modal} hideCb={this.hideModal} />
       </div>
     );
